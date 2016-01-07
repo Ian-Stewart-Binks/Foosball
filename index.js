7909e4b1dc9b9286a03305f8d3fcf7b7cb008c13
Ian Stewart-Binks <istewart@redhat.com>
2016-01-07 14:37:26 -0500

    Listen on openshift ip
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var http    = require('http');
     express = require('express'),
     app     = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT);
+app.listen(port, ip);
 
