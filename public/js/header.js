06c4e4d264fab755fd76c3481670d8c2b02828ce
Ian Stewart-Binks <istewart@redhat.com>
2016-01-12 15:36:44 -0500

    Clean up
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- public/js/header.js
+++ public/js/header.js
@@ -7,26 +7,20 @@ var headerStyle = {
 var Header = React.createClass({
     render: function() {
         return (
-          <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Foosball</a>
-          </div>
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">Foosball</a>
+    </div>
 
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li><a href="/"> Home <span className="sr-only">(current)</span></a></li>
-              <li><a href="/foosball_rules.html"> Rules </a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+      <li><a href="/"> Home <span className="sr-only">(current)</span></a></li>
+      <li><a href="/foosball_rules.html"> Rules </a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
         );
     }
 });
