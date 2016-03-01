46b4b6988b00eb49fdd75b974d83094d9d9c3b8f
Ian Stewart-Binks <istewart@redhat.com>
2016-03-01 16:46:02 -0500

    Add sqlite
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    sqlite3 = require('sqlite3').verbose(),
+    db      = new sqlite3.Database('mydb.db');
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -7,4 +9,20 @@ var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, ip);
+
+db.serialize(function() {
+
+  db.run("CREATE TABLE if not exists foosball_rules (rule TEXT)");
+  var stmt = db.prepare("INSERT INTO foosball_rules VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, rule FROM foosball_rules", function(err, row) {
+      console.log(row.id + ": " + row.rule);
+  });
+});
+
+db.close();
 
